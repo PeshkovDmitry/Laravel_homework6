fc12ecd79812715aadaee1f097969b2af2b0184d
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-12-01 12:58:59 +0300

    Сделал форму

---
+++ resources/views/form.blade.php
@@ -0,0 +1,22 @@
+<html>
+    <form action="" method="post">
+        <div>
+            <label for="title">Название</label>
+            <input type="text" name="title" id="title" required>
+        </div>
+        <div>
+            <label for="author">Название</label>
+            <input type="text" name="author" id="author" required>
+        </div>
+        <div>
+            <label for="genre">Жанр</label>    
+            <select name="genre" id="genre">
+                <option value="fantasy">Фэнтези</option>
+                <option value="sci-fi">Научная фантастика</option>
+                <option value="mystery">Мистика</option>
+                <option value="drama">Драма</option>
+            </select>
+            <button type="submit">Отправить</button>
+        </div>      
+    </form>
+</html>    
